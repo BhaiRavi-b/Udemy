065c7bf0670ab5c83d2bcb94937bb49861b3efd6
Akshay Kulkarni <akshayarvindkulkarni@gmail.com>
2024-01-31 16:52:04 +0530

    why

---
+++ Udemy Tutorial/CSS Tutorial/11.2+Bootstrap+Components/11.2 Bootstrap Components/test.js
@@ -0,0 +1,11 @@
+prompt("waht is ypur name");
+prompt("what is their name");
+
+var lovescore = Math.random() * 100;
+lovescore = Math.floor(lovescore) + 1;
+
+if (lovescore > 70) {
+  alert("your love score is " + lovescore + " Get a room guys");
+} else {
+  alert("your love score is " + lovescore);
+}
